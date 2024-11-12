7b4d86e2abbbbbc86941e19616d2675d2396257c
Dang <56210432+looks32@users.noreply.github.com>
2024-11-12 22:04:36 +0900

    server 수정

--- src/server.js
+++ src/server.js
@@ -3,9 +3,19 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleHome = () => console.log('누군가 home에 왔다.')
+// const handleHome = () => console.log('누군가 home에 왔다.')
+const handleHome = (req, res) => {
+	// console.log(req);
+	// console.log(res);
+	return res.send('I still love you');
+}
+
+const handleLogin = (req, res) => {
+	return res.send('Login here');
+}
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Go to http://localhost:${PORT}`);
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
