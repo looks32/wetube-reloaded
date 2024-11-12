f4b5878a8b8496203485d4bb86fa1922c477281c
Dang <56210432+looks32@users.noreply.github.com>
2024-11-12 16:15:28 +0900

    get 추가

--- src/server.js
+++ src/server.js
@@ -2,5 +2,10 @@ import express from "express";
 
 const PORT = 4000;
 const app = express();
+
+const handleHome = () => console.log('누군가 home에 왔다.')
+
+app.get("/", handleHome);
+
 const handleListening = () => console.log(`Go to http://localhost:${PORT}`);
 app.listen(PORT, handleListening)
