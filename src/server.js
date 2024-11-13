9e85444f2ec5187602ea3101a69b9280a19c305d
Dang <56210432+looks32@users.noreply.github.com>
2024-11-13 13:25:26 +0900

    server 수정

--- src/server.js
+++ src/server.js
@@ -3,6 +3,20 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+}
+
+// const privateMiddleware = (req, res, next) => {
+// 	const url = req.url;
+// 	if (url === "/protected"){
+// 		return res.send("<h1>NOT ALLOWED</h1>")
+// 	}
+// 	console.log('통과');
+// 	next();
+// }
+
 // const handleHome = () => console.log('누군가 home에 왔다.')
 const handleHome = (req, res) => {
 	// console.log(req);
@@ -14,8 +28,17 @@ const handleLogin = (req, res) => {
 	return res.send('Login here');
 }
 
+// const handleProtected = (req, res) => {
+// 	return res.send('welcome to the private lounge.')
+// }
+
+app.use(logger);
+// app.use(privateMiddleware);
+
+// app.get("/", logger, handleHome);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+// app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Go to http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
