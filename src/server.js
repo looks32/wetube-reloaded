2bb259a29d492404a3fac62ec22197020c7e4e48
Dang <56210432+looks32@users.noreply.github.com>
2024-11-15 11:55:20 +0900

    주석 제거 및 Routes만들기

--- src/server.js
+++ src/server.js
@@ -5,45 +5,24 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-
-
-// const logger = (req, res, next) => {
-// 	console.log(`${req.method} ${req.url}`);
-// 	next();
-// }
-
-// const privateMiddleware = (req, res, next) => {
-// 	const url = req.url;
-// 	if (url === "/protected"){
-// 		return res.send("<h1>NOT ALLOWED</h1>")
-// 	}
-// 	console.log('통과');
-// 	next();
-// }
-
-// const handleHome = () => console.log('누군가 home에 왔다.')
-const handleHome = (req, res) => {
-	// console.log(req);
-	// console.log(res);
-	return res.send('I still love you');
-}
-
-const handleLogin = (req, res) => {
-	return res.send('Login here');
-}
-
-// const handleProtected = (req, res) => {
-// 	return res.send('welcome to the private lounge.')
-// }
-
-// app.use(logger);
-// app.use(privateMiddleware);
 app.use(logger);
 
-// app.get("/", logger, handleHome);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-// app.get("/protected", handleProtected);
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home")
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 
 const handleListening = () => console.log(`Go to http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
