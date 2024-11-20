4464e1c697aadf6a8b1b4b98ff6f68fc609a1ae9
Dang <56210432+looks32@users.noreply.github.com>
2024-11-20 13:18:25 +0900

    반복문 기본

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -1,10 +1,7 @@
-const fakeUser = {
-  username : 'nico',
-  loggedIn : true,
+export const trending = (req, res) => {
+  const videos = [1,2,3,4,5,6,7,8,9,10]
+  return res.render("home", {pageTitle:"Home", videos});
 }
-
-
-export const trending = (req, res) => res.render("home", {pageTitle:"Home" , fakeUser});
 export const see = (req, res) => res.render("watch", {pageTitle:"Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
 
