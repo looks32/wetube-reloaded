f67d3c98e89d9f0e900b564c5f8173f348ed292e
Dang <56210432+looks32@users.noreply.github.com>
2024-11-21 15:49:39 +0900

    id를 통한 데이터전달

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -1,33 +1,46 @@
+
+let videos = [
+  {
+    title : "First Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 1
+  },
+  {
+    title : "Second Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 2
+  },
+  {
+    title : "Third Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 3
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title : "First Video",
-      rating : 5,
-      comments : 2,
-      createdAt : "2 minutes ago",
-      views : 59,
-      id : 1
-    },
-    {
-      title : "Second Video",
-      rating : 5,
-      comments : 2,
-      createdAt : "2 minutes ago",
-      views : 59,
-      id : 2
-    },
-    {
-      title : "Third Video3",
-      rating : 5,
-      comments : 2,
-      createdAt : "2 minutes ago",
-      views : 59,
-      id : 3
-    },
-  ]
   return res.render("home", {pageTitle:"Home", videos});
 }
-export const see = (req, res) => res.render("watch", {pageTitle:"Watch"});
+
+export const see = (req, res) => {
+  const { id } = req.params;
+  // 아래의 방법으로도 표기가능
+  // const id = req.params.id;
+  
+  const video = videos[id-1];
+  // 배열은 0부터 시작 id는 1부터 시작 이기때문에 -1을 해준다.
+
+  return res.render("watch", {pageTitle:`Watching ${video.title}`});
+};
+
 export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
 
 export const search = (req, res) => res.send("Search");
