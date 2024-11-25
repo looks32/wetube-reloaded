352eac1a00a5b8fcb6af706f9f87ce1c68d80658
Dang <56210432+looks32@users.noreply.github.com>
2024-11-25 16:10:51 +0900

    video 추가하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -55,3 +55,21 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 }
+
+export const getUpload = (req, res) => {
+  return res.render("upload", {pageTitle:"Upload Video"});
+}
+
+export const postUpload = (req, res) => {
+  const {title} = req.body;
+  const newVideo = {
+    title,
+    rating : 9,
+    comments : 9,
+    createdAt : "just now",
+    views : 0,
+    id : videos.length + 1.
+  }
+  videos.push(newVideo);
+  return res.redirect("/");
+}
