dd6d7b40561410c270641d54a25ccb31759647d2
Dang <56210432+looks32@users.noreply.github.com>
2024-11-27 00:06:13 +0900

    mongoose버전 하락, mongoose db연결, video 모델 연결

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -1,75 +1,33 @@
+import Video from "../models/Video";
 
-let videos = [
-  {
-    title : "First Video",
-    rating : 5,
-    comments : 2,
-    createdAt : "2 minutes ago",
-    views : 1,
-    id : 1
-  },
-  {
-    title : "Second Video",
-    rating : 5,
-    comments : 2,
-    createdAt : "2 minutes ago",
-    views : 59,
-    id : 2
-  },
-  {
-    title : "Third Video",
-    rating : 5,
-    comments : 2,
-    createdAt : "2 minutes ago",
-    views : 59,
-    id : 3
-  },
-];
 
-export const trending = (req, res) => {
-  return res.render("home", {pageTitle:"Home", videos});
-}
 
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {
+    console.log("errors", error);
+    console.log("videos", videos);
+  });
+  return res.render("home", { pageTitle: "Home", videos:[] });
+};
 export const watch = (req, res) => {
   const { id } = req.params;
-  // 아래의 방법으로도 표기가능
-  // const id = req.params.id;
-  
-  const video = videos[id-1];
-  // 배열은 0부터 시작 id는 1부터 시작 이기때문에 -1을 해준다.
-
-  return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
+  return res.render("watch", { pageTitle: `Watching` });
 };
-
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
-
 export const postEdit = (req, res) => {
   const { id } = req.params;
-  // console.log(req.body);
   const { title } = req.body;
-
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
-}
+};
 
 export const getUpload = (req, res) => {
-  return res.render("upload", {pageTitle:"Upload Video"});
-}
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
 
 export const postUpload = (req, res) => {
-  const {title} = req.body;
-  const newVideo = {
-    title,
-    rating : 9,
-    comments : 9,
-    createdAt : "just now",
-    views : 0,
-    id : videos.length + 1.
-  }
-  videos.push(newVideo);
+  const { title } = req.body;
   return res.redirect("/");
-}
+};
