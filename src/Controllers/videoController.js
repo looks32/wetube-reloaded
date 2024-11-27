4b1a70f65dbec457632a9fb0a8abf59f90d23baf
Dang <56210432+looks32@users.noreply.github.com>
2024-11-27 22:52:33 +0900

    db 추가 기본 작업

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -1,14 +1,10 @@
 import Video from "../models/Video";
 
+export const home = async(req, res) => { 
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
+}
 
-
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    console.log("errors", error);
-    console.log("videos", videos);
-  });
-  return res.render("home", { pageTitle: "Home", videos:[] });
-};
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
