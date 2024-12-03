64bf947b4029c50c5d509f303decf47c2f96a48d
Dang <56210432+looks32@users.noreply.github.com>
2024-12-03 16:32:56 +0900

    로그인 준비

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,4 +1,3 @@
-
 import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -6,18 +5,19 @@ export const postJoin = async (req, res) => {
     const { name, username, email, password, password2, location } = req.body;
     const pageTitle = "Join";
     if (password !== password2) {
-        return res.status(400).render("join", {
+    return res.status(400).render("join", {
         pageTitle,
         errorMessage: "Password confirmation does not match.",
     });
     }
     const exists = await User.exists({ $or: [{ username }, { email }] });
     if (exists) {
-        return res.status(400).render("join", {
+    return res.status(400).render("join", {
         pageTitle,
         errorMessage: "This username/email is already taken.",
-        });
+    });
     }
+    try {
     await User.create({
         name,
         username,
@@ -26,9 +26,30 @@ export const postJoin = async (req, res) => {
         location,
     });
     return res.redirect("/login");
+    } catch (error) {
+    return res.status(400).render("join", {
+        pageTitle: "Upload Video",
+        errorMessage: error._message,
+    });
+    }
 };
-export const edit = (req, res) => res.send("Edit User"); 
+
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const exists = await User.exists({ username });
+    if (!exists) {
+    return res.status(400).render("login", {
+        ageTitle: "Login",
+        errorMessage: "An account with this username does not exists.",
+    });
+    }
+    // check if password correct
+    res.end();
+};
+
+export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
