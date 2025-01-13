deb4dcb6ab08811622fe673e80c517e757f06730
Dang <56210432+looks32@users.noreply.github.com>
2025-01-14 00:23:29 +0900

    #17.1 Upgrading Dependencies 의 일부만 변경

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex:true,
-});
+mongoose.connect(process.env.DB_URL);
+
+// mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	useFindAndModify: false,
+// 	useCreateIndex:true,
+// });
 
 
 const db = mongoose.connection;
