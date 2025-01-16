1319d4faf0a2b6a3ffdd1bab7a898ac18c9b4dec
Dang <wheodnjs2@naver.com>
2025-01-16 15:20:50 +0900

    db 수정

--- src/db.js
+++ src/db.js
@@ -1,17 +1,16 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-// mongoose.connect(process.env.DB_URL);
-
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex:true,
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
-
 const db = mongoose.connection;
+
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("❌ DB Error", error);
+
 db.on("error", handleError);
 db.once("open", handleOpen);
